9670c6bae2aeb151874f0a85df93be2e5eaf7e9c
jz4github <44500032+jz4github@users.noreply.github.com>
2020-11-07 16:31:11 +1030

    Update InstallWebServer.ps1

--- InstallWebServer.ps1
+++ InstallWebServer.ps1
@@ -1,8 +1,10 @@
+param ($customerName)
+
 Install-WindowsFeature -name Web-Server -IncludeManagementTools
 $iisStartFile = "C:\inetpub\wwwroot\iisstart.htm"
 if (Test-Path $iisStartFile )
 {
-remove-item  $iisStartFile   -ErrorAction Ignore
+remove-item  $iisStartFile -ErrorAction Ignore
 }
 $content = "Greeting from " + $env:computername + ". We love Dtex! From " + $customerName
 Add-Content -Path $iisStartFile  -Value $content
